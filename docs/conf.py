c0cea6de82e50f874496706031d661ad1847e926
John (Juan) Lema <jlema@users.noreply.github.com>
2020-09-10 22:23:03 +0200

    Changed Read the Docs master file to index.rst (#2)

--- docs/conf.py
+++ docs/conf.py
@@ -59,3 +59,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Change Read the Docs master file to index.rst 
+master_doc = 'index'
