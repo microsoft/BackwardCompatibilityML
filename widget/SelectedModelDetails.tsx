4ae2ab0aa52cba593c2bcdfb7a8a07decca580ca
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-10-05 16:33:52 -0700

    Styling of the SelectedModel comonent

--- widget/SelectedModelDetails.tsx
+++ widget/SelectedModelDetails.tsx
@@ -23,12 +23,13 @@ class SelectedModelDetails extends Component<SelectedModelDetailsProps, null> {
 
     return (
       <div className="model-details">
-        <div>Selected Model Details</div>
-        <div>Model h2: {h2ModelName}</div>
-        <div>BTC score: {this.props.btc.toFixed(3)}</div>
-        <div>BEC score: {this.props.bec.toFixed(3)}</div>
-        <div>Performance: {this.props.performance.toFixed(3)}</div>
-        <div>lambda_c: {this.props.lambda_c.toFixed(2)}</div>
+        <div className="model-details-row">
+          <div className="model-details-info"><div className="model-details-info-data">{h2ModelName}</div><div className="model-details-info-type">(h2)</div></div>
+          <div className="model-details-info"><div className="model-details-info-data">{this.props.lambda_c.toFixed(2)}</div><div className="model-details-info-type">(lambda_c)</div></div>
+          <div className="model-details-info"><div className="model-details-info-data">{this.props.performance.toFixed(3)}</div><div className="model-details-info-type">(Performance)</div></div>
+          <div className="model-details-info"><div className="model-details-info-data">{this.props.btc.toFixed(3)}</div><div className="model-details-info-type">(BTC)</div></div>
+          <div className="model-details-info"><div className="model-details-info-data">{this.props.bec.toFixed(3)}</div><div className="model-details-info-type">(BEC)</div></div>
+        </div>
       </div>
     );
   }
