c48d857e3670dc824729ff241806ebd8c514358b
Nicholas King <v-nicki@microsoft.com>
2020-10-08 17:06:21 -0700

    2 build targets for webpack - jupyter and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+const jupyterConfig = {
   entry: ['./widget/index.tsx'],
   devtool: 'eval-source-map',
   performance: {
@@ -11,6 +13,9 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'widget.css'
     })
   ],
   module: {
@@ -29,6 +34,13 @@ module.exports = {
             enforce: "pre",
             test: /\.js$/,
             loader: "source-map-loader"
+        },
+        {
+            test: /\.css$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader'
+            ]
         }
     ]
   },
@@ -36,8 +48,65 @@ module.exports = {
     extensions: ['*', '.js', '.jsx', '*.ts', '*.tsx']
   },
   output: {
-    path: __dirname + '/backwardcompatibilityml/widget/resources',
+    path: path.resolve(__dirname, 'backwardcompatibilityml/widget/resources'),
     publicPath: '/',
     filename: 'widget-build.js'
   }
 };
+
+const devConfig = {
+  entry: [
+    'react-hot-loader/patch',
+    './widget/index.tsx'
+  ],
+  devtool: 'eval-source-map',
+  performance: {
+    hints: false
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [  
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'widget.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+      },
+      {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '*.ts', '*.tsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'widget-build.js'
+  },
+};
+
+module.exports = [ jupyterConfig, devConfig ]
