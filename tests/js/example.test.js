a60d0aa2becaae2faf0ea3ed71435f5fa4962c6e
Nicholas King <v-nicki@microsoft.com>
2020-10-20 12:57:33 -0700

    Jest setup in package.json. Move tests to their own folder.

--- tests/js/example.test.js
+++ tests/js/example.test.js
@@ -5,7 +5,7 @@ import { setupServer } from 'msw/node'
 */
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import RawValues from './widget/RawValues'
+import RawValues from 'RawValues'
 import { TestScheduler } from 'jest'
 import { act } from 'react-dom/test-utils'
 import { unmountComponentAtNode } from 'react-dom'
