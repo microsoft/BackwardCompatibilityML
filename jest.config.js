acea7353c38aed2a855beaa2300b646a31180829
Nicholas King <nicholas.b.king@vanderbilt.edu>
2020-10-23 13:52:28 -0700

    Test setup (#30)
    * First experiments in setting up test framework
    
    * Basic React widget test passing
    
    * Jest setup in package.json. Move tests to their own folder.
    
    * Remove jest.config.ts
    
    * Basic DataSelector test
    
    * Remove sum.js used in testing test setup
    
    * Configure ts-jest
    
    * Rename main test file
    
    * Added test for 'select all' checkboxes in DataSelector
    
    * Remove unnecessary code
    
    * Add UI test info to the Readme
    
    Co-authored-by: Nicholas King <v-nicki@microsoft.com>

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  verbose: true,
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: '<rootDir>/' })
+};
