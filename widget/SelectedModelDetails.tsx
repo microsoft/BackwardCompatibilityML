956ff36c6ff4f2546b6c4b5cfc836c4ba0c94756
Nicholas King <nicholas.b.king@vanderbilt.edu>
2020-11-04 11:18:13 -0800

    Rework raw values section (#46)
    * Change SelectedModelDetails to use a table, rework styles, remove RawValues files that are not used.
    
    * Revise styling based on design review
    
    * Fix text centering
    
    * Fix double negative sign issue
    
    * Remove !important from styles
    
    Co-authored-by: Nicholas King <v-nicki@microsoft.com>

--- widget/SelectedModelDetails.tsx
+++ widget/SelectedModelDetails.tsx
@@ -9,8 +9,10 @@ import * as d3 from "d3";
 type SelectedModelDetailsProps = {
   btc: number,
   bec: number,
-  performance: number,
-  lambda_c: number
+  h1Performance: number,
+  h2Performance: number,
+  performanceMetric: string,
+  lambdaC: number
 }
 
 class SelectedModelDetails extends Component<SelectedModelDetailsProps, null> {
@@ -19,19 +21,22 @@ class SelectedModelDetails extends Component<SelectedModelDetailsProps, null> {
   }
 
   render() {
-    var h2ModelName = "MyModel";
+    let deltaPerf = this.props.h1Performance != 0 ? (this.props.h2Performance - this.props.h1Performance) / this.props.h1Performance * 100 : 0;
 
     return (
-      <div className="model-details">
-        <div className="model-details-row">
-          <div className="model-details-info"><div className="model-details-info-data">{h2ModelName}</div><div className="model-details-info-type">(h2)</div></div>
-          <div className="model-details-info"><div className="model-details-info-data">{this.props.lambda_c.toFixed(2)}</div><div className="model-details-info-type">(lambda_c)</div></div>
-          <div className="model-details-info"><div className="model-details-info-data">{this.props.performance.toFixed(3)}</div><div className="model-details-info-type">(Performance)</div></div>
-          <div className="model-details-info"><div className="model-details-info-data">{this.props.btc.toFixed(3)}</div><div className="model-details-info-type">(BTC)</div></div>
-          <div className="model-details-info"><div className="model-details-info-data">{this.props.bec.toFixed(3)}</div><div className="model-details-info-type">(BEC)</div></div>
-        </div>
-      </div>
-    );
+      <table className="model-details" id="raw-values-table">
+        <tr className="model-details-row">
+          <td><span className="model-details-info-type">BTC</span>&nbsp;<span className="model-details-info-data">{this.props.btc.toFixed(3)}</span></td>
+          <td><span className="model-details-info-type">h1 {this.props.performanceMetric}</span>&nbsp;<span className="model-details-info-data">{this.props.h1Performance.toFixed(3)}</span></td>
+          <td><span className="model-details-info-type">Δ{this.props.performanceMetric}</span>&nbsp;<span className="model-details-info-data">{(deltaPerf >= 0 ? "+" : "") + deltaPerf.toFixed(3)}%</span></td>
+        </tr>
+        <tr className="model-details-row">
+          <td><span className="model-details-info-type">BEC</span>&nbsp;<span className="model-details-info-data">{this.props.bec.toFixed(3)}</span></td>
+          <td><span className="model-details-info-type">h2 {this.props.performanceMetric}</span>&nbsp;<span className="model-details-info-data">{this.props.h2Performance.toFixed(3)}</span></td>
+          <td><span className="model-details-info-type">λ<sub>c</sub></span>&nbsp;<span className="model-details-info-data">{this.props.lambdaC.toFixed(2)}</span></td>
+        </tr>
+      </table>
+    )
   }
 }
 export default SelectedModelDetails;
