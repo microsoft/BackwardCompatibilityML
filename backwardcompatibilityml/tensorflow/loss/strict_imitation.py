25a54d09b89cc52fe36a7973b7162236e18f1b83
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-12-16 12:09:50 -0800

    Add backward compatibility loss functions for new error and strict imitation under Tensorflow implmentation together with example notebooks

--- backwardcompatibilityml/tensorflow/loss/strict_imitation.py
+++ backwardcompatibilityml/tensorflow/loss/strict_imitation.py
@@ -1,13 +1,14 @@
 import tensorflow.compat.v2 as tf
+import tensorflow.compat.v1 as tf1
 
 
-class BCNLLLoss(object):
+class BCStrictImitationNLLLoss(object):
 
     def __init__(self, h1, h2, lambda_c):
         self.h1 = h1
         self.h2 = h2
         self.lambda_c = lambda_c
-        self.__name__ = "BCNLLLoss"
+        self.__name__ = "BCStrictImitationNLLLoss"
 
     def nll_loss(self, target_labels, model_output):
         model_output_labels = tf.argmax(model_output, axis=1)
@@ -20,64 +21,8 @@ class BCNLLLoss(object):
         return loss
 
     def dissonance(self, h2_output, target_labels):
-        nll_loss = self.nll_loss(target_labels, h2_output)
-        return nll_loss
-
-    def __call__(self, x, y):
-        h1_output = tf.argmax(self.h1(x), axis=1)
-        h2_output = self.h2(x)
-        h1_diff = h1_output - y
-        h1_correct = (h1_diff == 0)
-        _, x_support = tf.dynamic_partition(x, tf.dtypes.cast(h1_correct, tf.int32), 2)
-        _, y_support = tf.dynamic_partition(y, tf.dtypes.cast(h1_correct, tf.int32), 2)
-        h2_support_output = self.h2(x_support)
-        dissonance = self.dissonance(h2_support_output, y_support)
-        new_error_loss = self.nll_loss(y, h2_output) + self.lambda_c * dissonance
-
-        return tf.reduce_sum(new_error_loss)
-
-
-class BCCrossEntropyLoss(object):
-
-    def __init__(self, h1, h2, lambda_c):
-        self.h1 = h1
-        self.h2 = h2
-        self.lambda_c = lambda_c
-        self.__name__ = "BCCrossEntropyLoss"
-        self.cce_loss = tf.keras.losses.SparseCategoricalCrossentropy(
-            reduction=tf.keras.losses.Reduction.SUM)
-
-    def dissonance(self, h2_output, target_labels):
-        cross_entropy_loss = self.cce_loss(target_labels, h2_output)
-        return cross_entropy_loss
-
-    def __call__(self, x, y):
-        h1_output = tf.argmax(self.h1(x), axis=1)
-        h2_output = self.h2(x)
-        h1_diff = h1_output - y
-        h1_correct = (h1_diff == 0)
-        _, x_support = tf.dynamic_partition(x, tf.dtypes.cast(h1_correct, tf.int32), 2)
-        _, y_support = tf.dynamic_partition(y, tf.dtypes.cast(h1_correct, tf.int32), 2)
-        h2_support_output = self.h2(x_support)
-        dissonance = self.dissonance(h2_support_output, y_support)
-        new_error_loss = self.cce_loss(y, h2_output) + self.lambda_c * dissonance
-
-        return tf.reduce_sum(new_error_loss)
-
-
-class BCBinaryCrossEntropyLoss(object):
-
-    def __init__(self, h1, h2, lambda_c):
-        self.h1 = h1
-        self.h2 = h2
-        self.lambda_c = lambda_c
-        self.__name__ = "BCBinaryCrossEntropyLoss"
-        self.bce_loss = tf.keras.losses.BinaryCrossentropy(
-            reduction=tf.keras.losses.Reduction.SUM)
-
-    def dissonance(self, h2_output, target_labels):
-        cross_entropy_loss = self.bce_loss(target_labels, h2_output)
-        return cross_entropy_loss
+        log_loss = tf1.losses.log_loss(target_labels, h2_output, epsilon=1e-07)
+        return log_loss
 
     def __call__(self, x, y):
         h1_output = tf.argmax(self.h1(x), axis=1)
@@ -87,25 +32,81 @@ class BCBinaryCrossEntropyLoss(object):
         _, x_support = tf.dynamic_partition(x, tf.dtypes.cast(h1_correct, tf.int32), 2)
         _, y_support = tf.dynamic_partition(y, tf.dtypes.cast(h1_correct, tf.int32), 2)
         h2_support_output = self.h2(x_support)
-        dissonance = self.dissonance(h2_support_output, y_support)
-        new_error_loss = self.bce_loss(y, h2_output) + self.lambda_c * dissonance
+        strict_imitation_dissonance = self.dissonance(h2_support_output, y_support)
+        strict_imitation_loss = self.nll_loss(tf.argmax(y, axis=1), h2_output) + self.lambda_c * strict_imitation_dissonance
 
-        return tf.reduce_sum(new_error_loss)
+        return tf.reduce_sum(strict_imitation_loss)
 
 
-class BCKLDivLoss(object):
+class BCStrictImitationCrossEntropyLoss(object):
 
     def __init__(self, h1, h2, lambda_c):
         self.h1 = h1
         self.h2 = h2
         self.lambda_c = lambda_c
-        self.__name__ = "BCKLDivLoss"
+        self.__name__ = "BCStrictImitationCrossEntropyLoss"
+        self.cce_loss = tf.keras.losses.SparseCategoricalCrossentropy(
+            reduction=tf.keras.losses.Reduction.SUM)
+
+    def dissonance(self, h2_output, target_labels):
+        log_loss = tf1.losses.log_loss(target_labels, h2_output, epsilon=1e-07)
+        return log_loss
+
+    def __call__(self, x, y):
+        h1_output = tf.argmax(self.h1(x), axis=1)
+        h2_output = self.h2(x)
+        h1_diff = h1_output - tf.argmax(y, axis=1)
+        h1_correct = (h1_diff == 0)
+        _, x_support = tf.dynamic_partition(x, tf.dtypes.cast(h1_correct, tf.int32), 2)
+        _, y_support = tf.dynamic_partition(y, tf.dtypes.cast(h1_correct, tf.int32), 2)
+        h2_support_output = self.h2(x_support)
+        strict_imitation_dissonance = self.dissonance(h2_support_output, y_support)
+        strict_imitation_loss = self.cce_loss(tf.argmax(y, axis=1), h2_output) + self.lambda_c * strict_imitation_dissonance
+
+        return tf.reduce_sum(strict_imitation_loss)
+
+
+class BCStrictImitationBinaryCrossEntropyLoss(object):
+
+    def __init__(self, h1, h2, lambda_c):
+        self.h1 = h1
+        self.h2 = h2
+        self.lambda_c = lambda_c
+        self.__name__ = "BCStrictImitationBinaryCrossEntropyLoss"
+        self.bce_loss = tf.keras.losses.BinaryCrossentropy(
+            reduction=tf.keras.losses.Reduction.SUM)
+
+    def dissonance(self, h2_output, target_labels):
+        log_loss = tf1.losses.log_loss(target_labels, h2_output, epsilon=1e-07)
+        return log_loss
+
+    def __call__(self, x, y):
+        h1_output = tf.argmax(self.h1(x), axis=1)
+        h2_output = self.h2(x)
+        h1_diff = h1_output - tf.argmax(y, axis=1)
+        h1_correct = (h1_diff == 0)
+        _, x_support = tf.dynamic_partition(x, tf.dtypes.cast(h1_correct, tf.int32), 2)
+        _, y_support = tf.dynamic_partition(y, tf.dtypes.cast(h1_correct, tf.int32), 2)
+        h2_support_output = self.h2(x_support)
+        strict_imitation_dissonance = self.dissonance(h2_support_output, y_support)
+        strict_imitation_loss = self.bce_loss(y, h2_output) + self.lambda_c * strict_imitation_dissonance
+
+        return tf.reduce_sum(strict_imitation_loss)
+
+
+class BCStrictImitationKLDivLoss(object):
+
+    def __init__(self, h1, h2, lambda_c):
+        self.h1 = h1
+        self.h2 = h2
+        self.lambda_c = lambda_c
+        self.__name__ = "BCStrictImitationKLDivLoss"
         self.kldiv_loss = tf.keras.losses.KLDivergence(
             reduction=tf.keras.losses.Reduction.SUM)
 
     def dissonance(self, h2_output, target_labels):
-        kldiv_loss = self.kldiv_loss(target_labels, h2_output)
-        return kldiv_loss
+        log_loss = tf1.losses.log_loss(target_labels, h2_output, epsilon=1e-07)
+        return log_loss
 
     def __call__(self, x, y):
         h1_output = tf.argmax(self.h1(x), axis=1)
