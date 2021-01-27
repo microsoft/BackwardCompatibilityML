1e3d175735a398e839519fbad76290180b969485
ilmarinen <fernandes.praphat@gmail.com>
2021-01-27 12:01:17 -0800

    Add docstrings to Tensorflow models module, and add more Tensorflow documentation in topics section (#113)
    * Add docstrings to Tensorflow models module, and add more Tensorflow documentation in topics section
    
    * Add docstrings to backwardcompatibilityml.tensorflow.helpers.bc_fit
    
    * Bump version to v1.4.1
    
    * Documentation style changes
    
    * More edits

--- backwardcompatibilityml/tensorflow/helpers.py
+++ backwardcompatibilityml/tensorflow/helpers.py
@@ -2,6 +2,44 @@ import tensorflow.compat.v2 as tf
 
 
 def bc_fit(h2, training_set=None, testing_set=None, epochs=None, bc_loss=None, optimizer=None):
+    """
+    This function is used to train a model h2, using an instance of a Tensorflow BCLoss function
+    that has been instantiated using an existing model h1 and regularization parameter lambda_c.
+
+    Example usage:
+
+        h2 = tf.keras.models.Sequential([
+          tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+          tf.keras.layers.Dense(128,activation='relu'),
+          tf.keras.layers.Dense(10, activation='softmax')
+        ])
+
+        lambda_c = 0.9
+        h1.trainable = False
+        bc_loss = BCCrossEntropyLoss(h1, h2, lambda_c)
+
+        optimizer = tf.keras.optimizers.Adam(0.001)
+
+        tf_helpers.bc_fit(
+            h2,
+            training_set=ds_train,
+            testing_set=ds_test,
+            epochs=6,
+            bc_loss=bc_loss,
+            optimizer=optimizer)
+
+    Args:
+        h2: A Tensorflow model that we want to train using backward compatibility.
+        training_set: The training set for our model.
+        testing_set: The testing set for validating our model.
+        epochs: The number of training epochs.
+        bc_loss: An instance of a Tensorflow BCLoss function.
+        optimizer: The optimizer to use.
+
+    Returns:
+        Does not return anything. But it updates the weights of the model h2.
+    """
+
     @tf.function
     def compatibility_train_step(x_batch_train, y_batch_train, bc_loss, optimizer, h2):
         # Open a GradientTape to record the operations run
